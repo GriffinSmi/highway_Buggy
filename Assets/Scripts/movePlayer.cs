2d788b985c01283b147f96265b976711c7d7f4c1
Griffin Smith <griffin.123@comcast.net>
2023-04-10 17:06:39 -0400

    Final Push before turn in.

--- Assets/Scripts/movePlayer.cs
+++ Assets/Scripts/movePlayer.cs
@@ -7,7 +7,7 @@ public class movePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, -4);
+        GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, -7);
     }
 
     // Update is called once per frame
